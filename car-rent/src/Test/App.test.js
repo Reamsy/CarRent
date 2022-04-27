ef91561f14abbdcce29d067dd25ca9e87a17903d
Reamsy <szalanics.szabolcs@gmail.com>
2022-04-27 12:06:44 +0200

    test próbálkozások React

--- car-rent/src/Test/App.test.js
+++ car-rent/src/Test/App.test.js
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { AppRoutes } from '../views/AppRoutes';
 import { Login } from '../views/Login/Login';
+import userEvent from '@testing-library/user-event'
 
 
 test('Render Login Component', () => {
@@ -20,3 +21,32 @@ test('Render Login Component', () => {
 });
 
 //AXIOS TEST
+describe('Login Test', () => {
+
+  it('login axios', () => {
+
+    const fn = jest.fn();
+    const history = createMemoryHistory();
+    history.push('/');
+    render(
+      <AppRoutes history={history}>
+        <Login />
+      </AppRoutes>
+    );
+
+    const userNameInput = screen.getByTestId("UsernameTest");
+    const passwordInput = screen.getByTestId("PasswordTest");
+    const button = screen.getByTestId("testBTN");
+
+    userEvent.type(userNameInput, "user");
+    userEvent.type(passwordInput, "user");
+    userEvent.click(button);
+
+    expect(fn).toEqual({
+      username: "user",
+      password: "user"
+    });
+  });
+
+
+})
